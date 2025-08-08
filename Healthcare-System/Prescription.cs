9c837786bfd1c59914d18bed68aff850dfa70ccb
Eugenia Minnah <eaminnah@st.ug.edu.gh>
2025-08-08 15:42:58 +0000

    Implemented Patient and Prescription classes

---
+++ Healthcare-System/Prescription.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Healthcare_System
+{
+    public class Prescription
+    {
+        public int Id { get; set; }
+        public int PatientId { get; set; }
+        public string MedicationName { get; set; }
+        public DateTime DateIssued { get; set; }
+
+        public Prescription(int id, int patientId, string medicationName, DateTime dateIssued)
+        {
+            Id = id;
+            PatientId = patientId;
+            MedicationName = medicationName;
+            DateIssued = dateIssued;
+        }
+    }
+}

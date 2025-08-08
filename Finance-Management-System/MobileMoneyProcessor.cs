40f41bde1e46d90fbc897e1c47bd3c5d7be3862e
Eugenia Minnah <eaminnah@st.ug.edu.gh>
2025-08-08 14:49:40 +0000

    Implemented three Payment Processors

---
+++ Finance-Management-System/MobileMoneyProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance_Management_System
+{
+    public class MobileMoneyProcessor : ITransactionProcessor
+    {
+        public void Process(Transaction transaction)
+        {
+            Console.WriteLine($"[MobileMoney] Processing {transaction.Category} of GHS {transaction.Amount}");
+        }
+    }
+}

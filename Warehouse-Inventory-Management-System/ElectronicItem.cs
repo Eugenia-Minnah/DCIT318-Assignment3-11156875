ddcca0e1802fe4866eb45de8db1379f6b611b22b
Eugenia Minnah <eaminnah@st.ug.edu.gh>
2025-08-08 21:12:41 +0000

    Defined ElectronicItem and GroceryItem Classes

---
+++ Warehouse-Inventory-Management-System/ElectronicItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse_Inventory_Management_System
+{
+    public class ElectronicItem : IInventoryItem
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public int Quantity { get; set; }
+        public string Brand { get; }
+        public int WarrantyMonths { get; }
+
+        public ElectronicItem(int id, string name, int quantity, string brand, int warrantyMonths)
+        {
+            Id = id;
+            Name = name;
+            Quantity = quantity;
+            Brand = brand;
+            WarrantyMonths = warrantyMonths;
+        }
+
+        public override string ToString()
+            => $"ID:{Id} Name:{Name} Qty:{Quantity} Brand:{Brand} Warranty:{WarrantyMonths}mo";
+    }
+}

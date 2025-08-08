4ff1ee3d25f6a45d2cf362ca8e1cd9f388d9c500
Eugenia Minnah <eaminnah@st.ug.edu.gh>
2025-08-08 15:39:48 +0000

    Implemented  Generic Repository for Entity Management

---
+++ Healthcare-System/Repository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Healthcare_System
+{
+    public class Repository<T>
+    {
+        private List<T> items = new List<T>();
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public List<T> GetAll()
+        {
+            return items;
+        }
+
+        public T? GetById(Func<T, bool> predicate)
+        {
+            return items.FirstOrDefault(predicate);
+        }
+
+        public bool Remove(Func<T, bool> predicate)
+        {
+            var item = items.FirstOrDefault(predicate);
+            if (item != null)
+            {
+                items.Remove(item);
+                return true;
+            }
+            return false;
+        }
+    }
+}

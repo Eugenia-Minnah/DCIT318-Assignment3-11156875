a4ad7d5f9160b9422e03dec72a7175bb6b07741c
Eugenia Minnah <eaminnah@st.ug.edu.gh>
2025-08-08 21:48:25 +0000

    Implemented a Generic Inventory Repository

---
+++ Warehouse-Inventory-Management-System/InventoryRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse_Inventory_Management_System
+{
+    public class InventoryRepository<T> where T : IInventoryItem
+    {
+        private readonly Dictionary<int, T> _items = new Dictionary<int, T>();
+
+        public void AddItem(T item)
+        {
+            if (_items.ContainsKey(item.Id))
+                throw new DuplicateItemException($"Item with ID {item.Id} already exists.");
+            _items[item.Id] = item;
+        }
+
+        public T GetItemById(int id)
+        {
+            if (!_items.TryGetValue(id, out var item))
+                throw new ItemNotFoundException($"Item with ID {id} not found.");
+            return item;
+        }
+
+        public void RemoveItem(int id)
+        {
+            if (!_items.Remove(id))
+                throw new ItemNotFoundException($"Item with ID {id} not found.");
+        }
+
+        public List<T> GetAllItems()
+        {
+            return new List<T>(_items.Values);
+        }
+
+        public void UpdateQuantity(int id, int newQuantity)
+        {
+            if (newQuantity < 0)
+                throw new InvalidQuantityException("Quantity cannot be negative.");
+
+            if (!_items.TryGetValue(id, out var item))
+                throw new ItemNotFoundException($"Item with ID {id} not found.");
+
+            item.Quantity = newQuantity;
+        }
+    }
+}

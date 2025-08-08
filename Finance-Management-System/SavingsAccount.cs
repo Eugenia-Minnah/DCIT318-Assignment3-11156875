1f11c1ee0e691d26438fdee89275551ab32bca66
Eugenia Minnah <eaminnah@st.ug.edu.gh>
2025-08-08 14:57:39 +0000

    Created a General Account and a Savings Account

---
+++ Finance-Management-System/SavingsAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance_Management_System
+{
+    public sealed class SavingsAccount : Account
+    {
+        public SavingsAccount(string accountNumber, decimal initialBalance)
+            : base(accountNumber, initialBalance)
+        {
+        }
+
+        public override void ApplyTransaction(Transaction transaction)
+        {
+            if (transaction.Amount > Balance)
+            {
+                Console.WriteLine("Insufficient funds.");
+            }
+            else
+            {
+                Balance -= transaction.Amount;
+                Console.WriteLine($"Transaction successful. New balance: GHS {Balance}");
+            }
+        }
+    }
+}

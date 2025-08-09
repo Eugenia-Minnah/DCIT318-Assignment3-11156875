770a2a213673d675b8208f7437e85daa02dbd069
Eugenia Minnah <eaminnah@st.ug.edu.gh>
2025-08-09 15:11:20 +0000

    Added Student class to store student details and grades

---
+++ School-Grading-System/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_Grading_System
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int Score { get; set; }
+
+        public Student(int id, string fullName, int score)
+        {
+            Id = id;
+            FullName = fullName;
+            Score = score;
+        }
+
+        public string GetGrade()
+        {
+            if (Score >= 80 && Score <= 100)
+                return "A";
+            else if (Score >= 70)
+                return "B";
+            else if (Score >= 60)
+                return "C";
+            else if (Score >= 50)
+                return "D";
+            else
+                return "F";
+        }
+    }
+}
